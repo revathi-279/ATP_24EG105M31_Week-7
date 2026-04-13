bc243b6c7a21fe764098b22cb0ff4ffb8378d050
revathi-279 <revathi729070@gmail.com>
2026-04-13 21:12:24 +0530

    Week-7 BlogApp

--- BLOG-APP/backend/server.js
+++ BLOG-APP/backend/server.js
@@ -14,9 +14,11 @@ const app = exp()
 
 //Enable CORS
 app.use(cors({
-    origin:"*", // Accepts reqs from this origin
-    credentials:true                 // Enables to send the tokens back to the client
-}))
+  origin: true,
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 // Cookie parser middleware
 app.use(cookieParser())
