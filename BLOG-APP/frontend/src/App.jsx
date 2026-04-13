7bb11ebe53378a498fe068d4cc8809aa6a2b4056
Revathi <revathi729070@gmail.com>
2026-04-13 18:27:46 +0530

    Week-7 blogapp

--- BLOG-APP/frontend/src/App.jsx
+++ BLOG-APP/frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import { Toaster } from "react-hot-toast";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import AdminProfile from "./components/AdminProfile"; 
-import ArticleByID from "./components/ArticleByID";
 import Articles from "./components/Articles"; 
 import AuthorArticles from "./components/AuthorArticles";
+import ArticleById from "./components/ArticleById";
 import AuthorProfile from "./components/AuthorProfile";
 import EditArticle from "./components/EditArticle";
 import Home from "./components/Home";
